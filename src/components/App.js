66071a0322e02187abfa6bc9abf5420ae4d913e1
Sean <s410585038@gmail.com>
2021-01-22 01:37:47 +0800

    [change] course 11 code

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-import unsplash from '../api/unsplash'
-import ImageList from './imageList'
 
+// import youtubeAPI from '../api/youtube'
 class App extends React.Component {
-  state = { images: [] };
 
-  onSearchSubmit = async (term) => {
-    const response = await unsplash.get('search/photos', { params: { query: term } });
-
-    this.setState({ images: response.data.results });
-  };
-
+  onSearchSubmit = async(term) => {
+  }
   render() {
     return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList images={this.state.images}/>
+      <div className="ui container">
+        <SearchBar />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,28 @@
-import React from 'react';
+import React from 'react'
 
 class SearchBar extends React.Component {
-  state = { term: '' };
 
-  onFormSubmit = event => {
-    event.preventDefault();
+  state = { term: ''}
 
-    this.props.onSubmit(this.state.term);
-  };
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value })
+  }
 
+  onFormSubmit = (event) => {
+    event.preventDefault()
+  }
   render() {
     return (
-      <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
-            <label>Image Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={e => this.setState({ term: e.target.value })}
-            />
+            <label>Video Search</label>
+            <input type="text" value={this.state.term} onChange={this.onInputChange} />
           </div>
-        </form>
+        </form>  
       </div>
-    );
+    )
   }
 }
 
-export default SearchBar;
+export default SearchBar
