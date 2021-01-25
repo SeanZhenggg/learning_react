cced12147850de7981ebbe3a178b67e0f1feed8c
Sean <s410585038@gmail.com>
2021-01-26 00:50:38 +0800

    [change] course hook system

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState }from 'react'
 
 
-export default () => {
-  return ( <div>Accordion</div> )
+export default ({ items }) => {
+
+  const [activeIndex, setActiveIndex] = useState(null);
+  const onTitleClick = (index) => {
+    setActiveIndex(index === activeIndex ? null : index)
+  }
+
+  const renderedItems = items.map( (item, index) => {
+    const active = index === activeIndex ? 'active' : ''
+    return <React.Fragment key={item.title}>
+      <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+        <i className="dropdown icon"></i>
+        { item.title }
+      </div>
+      <div className={`content ${active}`}>
+        <p>{ item.content }</p>
+      </div>
+    </React.Fragment>
+  })
+  return ( <div className="ui styled accordion">{renderedItems}</div> )
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,26 @@
 import React from 'react'
-import Accordion from './Accordion'
+// import Accordion from './Accordion'
+import Search from './Search'
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework'
-  },
-  {
-    title: 'What use React?',
-    content: 'React favorite JS library among engineers'
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components'
-  },
-]
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end javascript framework'
+//   },
+//   {
+//     title: 'What use React?',
+//     content: 'React favorite JS library among engineers'
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React by creating components'
+//   },
+// ]
 export default () => {
   return (
     <div>
-      <Accordion />
+      <br />
+      <Search />
     </div>
   )
 }
