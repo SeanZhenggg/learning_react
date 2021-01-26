8506fadfd10f1fb7ea642182d0abd4be9a8d2e1b
Sean <s410585038@gmail.com>
2021-01-27 00:52:35 +0800

    [change] useEffect course

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,57 @@
 import React, { useState, useEffect } from 'react'
-// import axios from 'axios'
+import axios from 'axios'
 
 const Search = () => {
-  const [term, setTerm] = useState('')
+  const [term, setTerm] = useState('programming')
+  const [results, setResults] = useState([])
+
 
   useEffect(() => {
-    
-  }, [term])
+    const search = async() => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      })
+      setResults(data.query.search)
+    }
+
+    if(term && !results.length) {
+      search()
+    } else {
+      const timerId = setTimeout(() => {
+        if (term) {
+          search()
+        }
+      }, 1000)
+
+      return () => {
+        clearTimeout(timerId)
+      }
+    }
+  }, [results.length, term])
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet}}></span>
+        </div>
+      </div>
+    )
+  })
   return (
     <div>
       <div className="ui form">
@@ -19,6 +64,8 @@ const Search = () => {
           />
         </div>
       </div>
+
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   )
 }
