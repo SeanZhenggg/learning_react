319cb62e54eb5b97238f02709befa974022e7381
Sean <s410585038@gmail.com>
2021-01-31 23:11:43 +0800

    [change] course 14

---
+++ course/course14_Hooks in Practice/components/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import SearchBar from './SearchBar';
+import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
+
+import useVideos from '../hooks/useVideo'
+export default () => {
+  const [videos, search] = useVideos('buildings')
+  const [selectedVideo, setSelectedVideo] = useState(null)
+
+  useEffect(() => {
+    setSelectedVideo(videos[0])
+  }, [videos]) 
+
+
+  return (
+    <div className="ui container">
+      <SearchBar onSearchSubmit={search}/>
+      <div className="ui grid">
+        <div className="ui row">
+        <div className="eleven wide column">
+          <VideoDetail video={selectedVideo}/>
+        </div>
+        <div className="five wide column">
+          <VideoList onVideoSelect={(video) => setSelectedVideo(video)} videos={videos} />
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+}
