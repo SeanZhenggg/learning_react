ea119dbe616598f5e9467144926daad0b036cd22
SeanZhenggg <s410585038@gmail.com>
2021-02-20 18:31:16 +0800

    [change] course 22 - 322

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducers from './reducers'
 
+const composerEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducers, 
+  composerEnhancers(applyMiddleware())
+)
 // ClientId = 889878431283-jtbher2n3cevvo1l16rlk9eitgvacucl.apps.googleusercontent.com
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
